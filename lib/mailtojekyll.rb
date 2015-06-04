edad214fd1eb616f6005e13ef5066149e4b875c8
Kate Klemp <kklemp@misdepartment.com>
2015-06-04 08:10:57 -0500

    Move parse method to actual app, change tests to reference correctly

--- lib/mailtojekyll.rb
+++ lib/mailtojekyll.rb
@@ -1,4 +1,4 @@
-require 'emailparser'
+require 'mail'
 
 # use or mixin the emailparser module? or include the emailparser code here
 # loop through emails to create multiple Post objects (based on Post Class)
@@ -18,3 +18,28 @@ require 'emailparser'
 # body tests - check to make sure it exists, and check to make sure it is *exactly* what I want it to be (copy-paste the body of the email and format to final result, then code to make it pass)
 # what order do the images come in?
 # strip all html tags *except* img? Keep CID logic from JekyllMail?
+
+
+def parse(email)
+  unless email.nil? || email.empty?
+
+    email = Mail.read(email)
+
+    # parse the subject to create the title
+    @subject = email.subject
+    (@title, secret) = @subject.split(/\|\|/)
+    @title.strip!
+
+    raise StandardError, "No subject" if @subject.nil? || @subject.empty?
+
+    # parse the metadata to check for the secret and create additional key/value pairs
+    unless secret.nil?
+      (key, @secret) = secret.split(/:\s?/)
+      @secret.strip!
+    end
+
+    raise StandardError, "Secret incorrect" unless @secret == "jekyllmail"
+
+  end
+
+end
---
+++ spec/mailtojekyll_spec.rb
@@ -0,0 +1,66 @@
+require "mailtojekyll"
+
+describe 'mailtojekyll' do
+
+  describe 'parse' do
+
+    context 'given no emails' do
+      it 'returns nil' do
+        expect(parse("")).to be_nil
+      end
+    end
+
+    context 'given a single email' do
+
+      context 'from gmail' do
+
+        context 'sent as plain text' do
+          before(:each) do
+            parse('spec/mocks/gmail-plain-text.eml')
+          end
+          it 'parses the subject correctly' do
+            expect(instance_variable_get(:@subject)).to eq("Plain Text from Gmail || secret: jekyllmail")
+            expect(instance_variable_get(:@title)).to eq("Plain Text from Gmail")
+            expect(instance_variable_get(:@secret)).to eq("jekyllmail")
+          end
+        end
+
+        context 'sent as html' do
+          before(:each) do
+            parse('spec/mocks/gmail-html-format.eml')
+          end
+          it 'parses the subject correctly' do
+            expect(instance_variable_get(:@subject)).to eq("HTML formatting from Gmail || secret: jekyllmail")
+            expect(instance_variable_get(:@title)).to eq("HTML formatting from Gmail")
+            expect(instance_variable_get(:@secret)).to eq("jekyllmail")
+          end
+        end
+
+        context 'with incorrect secret' do
+          it 'exits the method with an error' do
+            expect{ parse('spec/mocks/gmail-wrong-secret.eml') }.to raise_error
+            expect(instance_variable_get(:@secret)).not_to eq("jekyllmail")
+          end
+        end
+
+        context 'with no secret' do
+          it 'exits the method with an error' do
+            expect{ parse('spec/mocks/gmail-no-secret.eml') }.to raise_error
+            expect(instance_variable_get(:@secret)).not_to eq("jekyllmail")
+          end
+        end
+
+        context 'with no subject' do
+          it 'exits the method with an error' do
+            expect{ parse('spec/mocks/gmail-no-subject.eml') }.to raise_error
+            expect(instance_variable_get(:@subject)).to be_nil
+          end
+        end
+
+      end # end of gmail context
+
+    end # end of single email context
+
+  end
+
+end
