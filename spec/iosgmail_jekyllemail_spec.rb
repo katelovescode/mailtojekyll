e7816741eaeb76c69ef8bdad100fb97099aac7d5
Kate Klemp <kklemp@misdepartment.com>
2015-06-09 16:01:01 -0500

    iOSGmail tests pass after refactor

--- spec/iosgmail_jekyllemail_spec.rb
+++ spec/iosgmail_jekyllemail_spec.rb
@@ -1,6 +1,6 @@
 require 'jekyllemail'
 
-describe JekyllEmail, disabled: true do
+describe JekyllEmail do
 
   context 'given a message sent from iOS Gmail App' do
     let(:path) { "spec/mocks/iosgmail/" }
