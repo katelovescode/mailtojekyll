9a635a2aeea00a6c8c433bd82deab84558a97070
Kate Klemp <kklemp@misdepartment.com>
2015-06-18 12:32:40 -0500

    Remove emoji tests

--- spec/jekyllemail/jekyllemail_owa_spec.rb
+++ spec/jekyllemail/jekyllemail_owa_spec.rb
@@ -90,12 +90,6 @@ describe JekyllEmail do
       it_behaves_like 'an email with no attachments'
     end
     
-    context 'with emoji' do
-      let(:filename) {'emoji'}
-      let(:thistitle) {'Emoji' + identification }
-      it_behaves_like 'an empty email'
-    end
-    
     context 'with bold-italic-underline formatting' do
       let(:filename) {'bold-italic-underline'}
       let(:thistitle) {'Bold italic underline' + identification }
